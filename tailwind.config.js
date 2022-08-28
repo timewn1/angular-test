32cad5ac3a8dadce1ee97908e50cb496f70aa7c9
pwbear1230 <timewornbn@gmail.com>
2022-08-28 02:20:15 -0300

    feat: add general theming for the tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,73 @@
 /** @type {import('tailwindcss').Config} */
+function range(start, end, increment = 1) {
+  const count = Math.floor((end - start + increment) / increment);
+  return Array(count)
+    .fill(0)
+    .map((_, idx) => start + idx * increment);
+}
+
+const minFontSize = 5;
+const maxFontSize = 80;
+
+const minSpacingPixel = 0;
+const maxSpacingPixel = 800;
+const spacingPixelIncrement = 5;
+
+const vhs = ['10vh', '20vh', '30vh', '40vh', '50vh', '60vh', '70vh', '80vh', '90vh', '100vh'];
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    // Extend default configurations
+    extend: {
+      colors: {},
+      container: {
+        center: true,
+      },
+    },
+    // Override default configurations
+    fontWeight: {
+      normal: 400,
+      medium: 600,
+      bold: 700,
+      black: 900,
+    },
+    fontSize: {
+      ...range(minFontSize, maxFontSize).reduce((merged, f) => ({ ...merged, [f]: `${f}px` }), {}),
+    },
+    spacing: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce(
+        (merged, f) => ({ ...merged, [f]: `${f}px` }),
+        {}
+      ),
+    },
+    maxWidth: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce(
+        (merged, f) => ({ ...merged, [f]: `${f}px` }),
+        {}
+      ),
+    },
+    minWidth: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce(
+        (merged, f) => ({ ...merged, [f]: `${f}px` }),
+        {}
+      ),
+    },
+    maxHeight: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce(
+        (merged, f) => ({ ...merged, [f]: `${f}px` }),
+        {}
+      ),
+      ...vhs.reduce((merged, vh) => ({ ...merged, [vh]: vh }), {}),
+    },
+    minHeight: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce(
+        (merged, f) => ({ ...merged, [f]: `${f}px` }),
+        {}
+      ),
+      ...vhs.reduce((merged, vh) => ({ ...merged, [vh]: vh }), {}),
+    },
   },
   plugins: [],
 };
