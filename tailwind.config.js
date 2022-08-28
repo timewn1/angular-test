c781763d62e45fc337420a8359cff605a479fda8
pwbear1230 <timewornbn@gmail.com>
2022-08-28 02:26:44 -0300

    feat: add colors to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,14 @@ module.exports = {
   theme: {
     // Extend default configurations
     extend: {
-      colors: {},
+      colors: {
+        primary: '#c1d1e8',
+        secondary: '#5692e8',
+        dark: '#182c47',
+        thick: '#0e1a2b',
+        'black-start': '#081221',
+        'black-end': '#03080f',
+      },
       container: {
         center: true,
       },
